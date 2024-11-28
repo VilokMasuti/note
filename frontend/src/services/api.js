81ca3bbb53fed6eaca51ff4004d99437673d5f65
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-11-28 09:57:22 +0530

    url update
    url update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://note-7pl0.onrender.com/api/notes'
+
 const api = axios.create({
   baseURL: API_URL,
 });
